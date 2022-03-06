79fd04df3c35743de65efada289baee1a24002d8
Paphawit Ngamchaliew <greatng@Paphawits-MacBook-Air.local>
2022-03-06 16:59:16 +0700

    Fix2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pngamcha <pngamcha@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:40:27 by pngamcha          #+#    #+#             */
-/*   Updated: 2022/03/06 16:55:06 by pngamcha         ###   ########.fr       */
+/*   Updated: 2022/03/06 16:59:06 by pngamcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static char	*get_line(char *content, size_t *pos)
 	j = 0;
 	while (content[i] != '\n' && content[i])
 		i++;
-	line = malloc(sizeof(char) * (i - (*pos) + 1));
+	line = malloc(sizeof(char) * (i - (*pos) + 2));
 	if (!line)
 		return (NULL);
 	while (*pos < i + 1)
